7acd0c33fe479624c714af12719234503055a3bf
Hugo Moreira <hugo.cesar91@gmail.com>
2025-07-28 21:25:12 -0300

    Adicionada documentação de listagem dos livros.

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import express from "express";
 import booksRoutes from "./routes/booksRoutes";
 import authorsRoutes from "./routes/authorsRoutes";
 import categoriesRoutes from "./routes/categoriesRoutes";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/books", booksRoutes);
 app.use("/authors", authorsRoutes);
